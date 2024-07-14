95860f04564752f6edad5956b433d97086bfaa2e
Bergkley <bergkley@gmail.com>
2024-07-13 23:22:11 -0300

    feat: estruturação  do componente Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,11 +8,27 @@ const Login = () => {
         <h1>Acesse o sistema</h1>
         <div>
         <input type="email" placeholder="E-mail" />
+        <FaUser className='icon' />
         </div>
         <div>
         <input type="password" placeholder="Senha" />
+        <FaLock className='icon' />
         </div>
+        <div className="recal-forget">
+            <label htmlFor="">
+                <input type="checkbox" />
+                Lembre de mim
+            </label>
+            <a href="#" target="_blank">Esqueceu sua senha?</a>
+        </div>
+
         <button>Entrar</button>
+
+        <div className="signup-link">
+            <p>
+                Ainda não tem uma conta? <a href="#">Registrar</a>
+            </p>
+            </div>
       </form>
     </div>
   )
