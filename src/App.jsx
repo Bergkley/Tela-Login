acdb38c2418ec06d6e65cc2e306edf8978e7cf3d
Bergkley <bergkley@gmail.com>
2024-07-13 23:34:10 -0300

    fix: refatoração

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
-import Login from './Components/Login/Login'
+import "./App.css";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Login />
     </div>
-  )
+  );
 }
-export default App
+export default App;
